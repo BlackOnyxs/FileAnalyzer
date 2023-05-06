e316ee48b5f065d7ac8da2123ada2ddd6250e112
BlackOnyxs <robinag2018@gmail.com>
2023-05-06 12:36:19 -0500

    Add project files.

---
+++ FileAnalyzer/Program.cs
@@ -0,0 +1,16 @@
+﻿class FileAnalyzer { 
+    static void Main(String[] args) {
+        Console.WriteLine("Ingrese la ruta");
+        string path = Console.ReadLine();
+
+        if ( !Directory.Exists(path) ) {
+            Console.WriteLine("La ruta no es válida");
+            return;
+        }
+        
+
+
+    }
+}
+
+class 
